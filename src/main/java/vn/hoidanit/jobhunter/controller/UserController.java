554e3d9801aef83e10bc0fa2a723461abd7c51b4
duynguyenhd209 <duynguyenhd209@gmail.com>
2025-09-12 06:48:27 +0700

    add handle user

---
+++ src/main/java/vn/hoidanit/jobhunter/controller/UserController.java
@@ -0,0 +1,27 @@
+package vn.hoidanit.jobhunter.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import vn.hoidanit.jobhunter.domain.User;
+import vn.hoidanit.jobhunter.service.UserService;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/user/create")
+    public User createNewUser(@RequestBody User user) {
+        User newUser = this.userService.handleCreateUser(user);
+        return newUser;
+    }
+
+}
