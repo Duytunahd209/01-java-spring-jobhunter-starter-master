756de8c7630a698b1342dfbc0317c19cd61fee59
duynguyenhd209 <duynguyenhd209@gmail.com>
2025-09-28 10:38:09 +0700

    add model resume

---
+++ src/main/java/vn/hoidanit/jobhunter/domain/Resume.java
@@ -0,0 +1,54 @@
+package vn.hoidanit.jobhunter.domain;
+
+import java.time.Instant;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Getter;
+import lombok.Setter;
+import vn.hoidanit.jobhunter.util.SecurityUtil;
+import vn.hoidanit.jobhunter.util.constant.ResumeStateEnum;
+
+@Getter
+@Setter
+public class Resume {
+    private long id;
+    private String email;
+    private String url;
+
+    @Enumerated(EnumType.STRING)
+    private ResumeStateEnum status;// PENDING-REVIEWING-APPROVED-REJECTED
+    private Instant createdAt;
+    private Instant updatedAt;
+    private String createdBy;
+    private String updatedBy;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "job_id")
+    private Job job;
+
+    @PrePersist
+    public void handleBeforeCreate() {
+        this.createdBy = SecurityUtil.getCurrentUserLogin().isPresent()
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+        this.createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void handleBeforeUpdate() {
+        this.updatedBy = SecurityUtil.getCurrentUserLogin().isPresent()
+                ? SecurityUtil.getCurrentUserLogin().get()
+                : "";
+        this.updatedAt = Instant.now();
+    }
+
+}
